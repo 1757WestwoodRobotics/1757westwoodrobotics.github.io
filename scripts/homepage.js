f33f3e4d1b923808b83c480401ac8d75d26da2e0
Larry Tseng <lt222888@gmail.com>
2017-07-24 12:48:21 -0400

    Add smoothscroll js and scrollmagic for animations and transitions

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,35 +1,53 @@
 // jQuery Document Ready Function
 $(document).ready(function() {
 
+    // Smooth Scroll
+
+    var scroll = new SmoothScroll('#home-scroll-down', {
+        speed: 1500,
+        easing: 'easeInOutCubic'
+
+    });
+
     // Homepage JSON Data
 
-    $.getJSON('robots/robot-list.json', function(data) {
-        data = data.slice(0, 4);    // Reduces the number of robots displayed to 4
+    $.getJSON('/media/news/news-list.json', function(data) {
+        data = data.slice(0, 3);    // Reduces the number of news articles displayed
+        var template = $('#news-template').html();
+        Mustache.parse(template);
+        $('#news-gallery').html(Mustache.render(template, data));
+    });
+
+    $.getJSON('/robots/robot-list.json', function(data) {
+        data = data.slice(0, 4);    // Reduces the number of robots displayed
         var template = $('#robot-gallery-template').html();
         Mustache.parse(template);
         $('#robot-gallery').html(Mustache.render(template, data));
     });
-    $.getJSON('community/sponsors/sponsor-list.json', function(data) {
+
+    $.getJSON('/community/sponsors/sponsor-list.json', function(data) {
         var template = $('#sponsors-template').html();
         Mustache.parse(template);
         $('#sponsors-container').html(Mustache.render(template, data));
 
-        animatePage();
+        animateRobotGallery();
     });
 
 
     // Animations
 
-    function animatePage() {
-        var controller = new ScrollMagic.Controller();
+    var controller = new ScrollMagic.Controller();
+
+    function animateRobotGallery() {
 
         var robotGalleryTween = TweenMax.staggerFrom($('.robot'), 1.5, {opacity: 0, left: ($(window).width() - $('.robot').width()), ease: Power2.easeInOut}, 0.15);
-
         var robotGalleryScene = new ScrollMagic.Scene({
-            triggerElement: '#home-news'
+            triggerElement: '#home-news',
+            reverse: false
         })
             .setTween(robotGalleryTween)
-            .addIndicators({name: "robot cards"})
-            .addTo(controller);
+            .addIndicators({name: "robot cards"});
+
+        controller.addScene(robotGalleryScene);
     }
 });
