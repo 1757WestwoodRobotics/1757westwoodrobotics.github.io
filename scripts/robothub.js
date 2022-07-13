aa49bf9802cbef48252af348f00d2f0379075d4d
Luke Maxwell <24lbmaxwell@protonmail.com>
2022-07-13 18:32:57 -0400

    landon gets what he wants!

--- scripts/robothub.js
+++ scripts/robothub.js
@@ -1,12 +1,8 @@
 // ready function
 $(document).ready(function () {
-  let bricklayer = new Bricklayer(document.querySelector(".bricklayer"));
-
   $.getJSON("/robots/robot-list.json", function (data) {
     let template = $("#robot-cards-template").html();
     Mustache.parse(template);
     $("#robot-deck").html(Mustache.render(template, data));
   });
-
-  bricklayer.redraw();
 });
