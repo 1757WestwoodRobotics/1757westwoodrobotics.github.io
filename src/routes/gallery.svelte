c100062e279c22132cd5e007ea4fdd420021789a
Luke Maxwell <24lbmaxwell@protonmail.com>
2022-07-16 16:23:49 -0400

    2017, 2018 images

--- src/routes/gallery.svelte
+++ src/routes/gallery.svelte
@@ -109,6 +109,87 @@
 			'/img/gallery/2019/IMG_2169.webp',
 			'/img/gallery/2019/IMG_5080.webp',
 			'/img/gallery/2019/IMG_5094.webp'
+		],
+		'2018': [
+			'/img/gallery/2018/IMG_2401.webp',
+			'/img/gallery/2018/IMG_2406.webp',
+			'/img/gallery/2018/IMG_2408.webp',
+			'/img/gallery/2018/IMG_2418.webp',
+			'/img/gallery/2018/IMG_2427.webp',
+			'/img/gallery/2018/IMG_2429.webp',
+			'/img/gallery/2018/IMG_4395.webp',
+			'/img/gallery/2018/IMG_4405.webp',
+			'/img/gallery/2018/IMG_4408.webp',
+			'/img/gallery/2018/IMG_4419.webp',
+			'/img/gallery/2018/IMG_4487.webp',
+			'/img/gallery/2018/IMG_4497.webp',
+			'/img/gallery/2018/IMG_4525.webp',
+			'/img/gallery/2018/IMG_4599.webp'
+		],
+		'2017': [
+			'/img/gallery/2017/33129322773_4f835ae615_o.webp',
+			'/img/gallery/2017/33812064451_b266426225_o.webp',
+			'/img/gallery/2017/DSC_8353.webp',
+			'/img/gallery/2017/DSC_8360.webp',
+			'/img/gallery/2017/DSC_8363.webp',
+			'/img/gallery/2017/DSC_8372.webp',
+			'/img/gallery/2017/DSC_8378.webp',
+			'/img/gallery/2017/DSC_8379.webp',
+			'/img/gallery/2017/DSC_8392.webp',
+			'/img/gallery/2017/DSC_8395.webp',
+			'/img/gallery/2017/DSC_8401.webp',
+			'/img/gallery/2017/DSC_8409.webp',
+			'/img/gallery/2017/DSC_8414.webp',
+			'/img/gallery/2017/DSC_8421.webp',
+			'/img/gallery/2017/DSC_8428.webp',
+			'/img/gallery/2017/DSC_8432.webp',
+			'/img/gallery/2017/DSC_8437.webp',
+			'/img/gallery/2017/DSC_8443.webp',
+			'/img/gallery/2017/DSC_8451.webp',
+			'/img/gallery/2017/DSC_8457.webp',
+			'/img/gallery/2017/DSC_8467.webp',
+			'/img/gallery/2017/DSC_8479.webp',
+			'/img/gallery/2017/DSC_8491.webp',
+			'/img/gallery/2017/DSC_8496.webp',
+			'/img/gallery/2017/DSC_8502.webp',
+			'/img/gallery/2017/DSC_8504.webp',
+			'/img/gallery/2017/DSC_8509.webp',
+			'/img/gallery/2017/DSC_8516.webp',
+			'/img/gallery/2017/DSC_8521.webp',
+			'/img/gallery/2017/DSC_8527.webp',
+			'/img/gallery/2017/DSC_8533.webp',
+			'/img/gallery/2017/DSC_8537.webp',
+			'/img/gallery/2017/DSC_8548.webp',
+			'/img/gallery/2017/DSC_8554.webp',
+			'/img/gallery/2017/DSC_8560.webp',
+			'/img/gallery/2017/DSC_8566.webp',
+			'/img/gallery/2017/DSC_8574.webp',
+			'/img/gallery/2017/DSC_8590.webp',
+			'/img/gallery/2017/DSC_8596.webp',
+			'/img/gallery/2017/DSC_8603.webp',
+			'/img/gallery/2017/DSC_8608.webp',
+			'/img/gallery/2017/DSC_8613.webp',
+			'/img/gallery/2017/DSC_8621.webp',
+			'/img/gallery/2017/DSC_8627.webp',
+			'/img/gallery/2017/DSC_8629.webp',
+			'/img/gallery/2017/DSC_8641.webp',
+			'/img/gallery/2017/DSC_8646.webp',
+			'/img/gallery/2017/DSC_8668.webp',
+			'/img/gallery/2017/FRCSmithfieldRI3-2017S(16).webp',
+			'/img/gallery/2017/FRCSmithfieldRI3-2017S(38).webp',
+			'/img/gallery/2017/FRCSmithfieldRI32017S(3).webp',
+			'/img/gallery/2017/FRCSmithfieldRI3-2017S(50).webp',
+			'/img/gallery/2017/FRCSmithfieldRI3-2017S(9).webp',
+			'/img/gallery/2017/FRCUNHChampionship4-2017(15).webp',
+			'/img/gallery/2017/FRCUNHChampionship4-2017(20).webp',
+			'/img/gallery/2017/FRCUNHChampionship4-2017(26).webp',
+			'/img/gallery/2017/FRCUNHChampionship4-2017(4).webp',
+			'/img/gallery/2017/IMG_4774.webp',
+			'/img/gallery/2017/IMG_4777.webp',
+			'/img/gallery/2017/IMG_4792.webp',
+			'/img/gallery/2017/IMG_4797.webp',
+			'/img/gallery/2017/IMG_4803.webp',
+			'/img/gallery/2017/IMG_4807.webp'
 		]
 	};
 </script>
@@ -147,6 +228,44 @@
 			let:loaded
 		>
 			{#each urls['2019'] as url, imageIndex}
+				<div class="mx-auto my-4 w-full h-full flex items-center justify-center align-center">
+					{#if loaded.includes(imageIndex)}
+						<img src={url} class="max-h-[70vh]" />
+					{/if}
+				</div>
+			{/each}
+		</svelte:component>
+	</div>
+	<h3 class="w-full text-center text-5xl">2018: Power Up</h3>
+	<div class="rounded-xl p-4 drop-shadow-sm bg-slate-800 m-4">
+		<svelte:component
+			this={Carousel}
+			bind:this={carousel}
+			autoplay
+			pauseOnFocus
+			autoplayProgressVisible
+			let:loaded
+		>
+			{#each urls['2018'] as url, imageIndex}
+				<div class="mx-auto my-4 w-full h-full flex items-center justify-center align-center">
+					{#if loaded.includes(imageIndex)}
+						<img src={url} class="max-h-[70vh]" />
+					{/if}
+				</div>
+			{/each}
+		</svelte:component>
+	</div>
+	<h3 class="w-full text-center text-5xl">2017: SteamWorks</h3>
+	<div class="rounded-xl p-4 drop-shadow-sm bg-slate-800 m-4">
+		<svelte:component
+			this={Carousel}
+			bind:this={carousel}
+			autoplay
+			pauseOnFocus
+			autoplayProgressVisible
+			let:loaded
+		>
+			{#each urls['2017'] as url, imageIndex}
 				<div class="mx-auto my-4 w-full h-full flex items-center justify-center align-center">
 					{#if loaded.includes(imageIndex)}
 						<img src={url} class="max-h-[70vh]" />
