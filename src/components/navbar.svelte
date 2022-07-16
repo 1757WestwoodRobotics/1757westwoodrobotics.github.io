47c6509667986244a2a51aceb38d4a113ce2ba4e
Luke Maxwell <24lbmaxwell@protonmail.com>
2022-07-16 11:55:53 -0400

    fix mobile navbar

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -114,7 +114,7 @@
 				<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
 				<a
 					href="/"
-					class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+					class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
 					aria-current="page">Home</a
 				>
 
