a01d412bf2d7aa054abb41dbba28918c387afa6d
Luke Maxwell <24lbmaxwell@protonmail.com>
2022-07-16 13:18:41 -0400

    better robot page

--- src/components/robotPage.svelte
+++ src/components/robotPage.svelte
@@ -9,15 +9,16 @@
 </script>
 
 <div class="bg-slate-900 flex flex-wrap overflow-auto p-5 h-full m-4 rounded-lg">
-	<span class="grow flex-col basis-5/12 flex justify-center items-center shrink">
+	<span class="grow flex-col md:basis-5/12 flex justify-center items-center shrink basis-full">
 		<img src={image} class="bg-[url('{image}')] aspect-auto" />
 	</span>
 	<div class="text-center w-full p-4 text-white flex-auto basis-7/12">
 		<h2 class="text-3xl">{year}{name !== '' ? ' - ' : ''}{name}</h2>
 		<p>{desc}</p>
+		<br>
 		{#if videos != ''}
 			<div
-				class="bottom-0 inset-x-0 text-gray-300 hover:underline hover:cursor-pointer text-xl"
+				class="bottom-0 inset-x-0 text-blue-400 underline hover:cursor-pointer text-xl"
 				on:click={() => {
 					showVideos = !showVideos;
 				}}
